fd57b2f1f95d04e39f80daaff202a00486d369a3
Jean-Etienne Poirrier <jepoirrier@gmail.com>
2013-12-23 00:46:23 +0100

    Rebuilding the json string (finally!), storing it and re-importing it
    rawToChar() saved my life :-) and cat() simply gets rid of backslashes
    in “\””

--- steps.R
+++ steps.R
@@ -21,5 +21,14 @@ sig = sign_oauth1.0(fbr, token=token$oauth_token, token_secret=token$oauth_token
 # Get the data (here: steps - I started using Fitbit on March 6th, 2013)
 steps = GET("http://api.fitbit.com/1/user/-/activities/steps/date/2013-03-06/2013-03-07.json",sig)
 
-dput(steps, "mySteps.dat")
+# Now rebuilding the json string
+# steps[6] contains each char in hexa --> translate into normal string
+myJson = rawToChar(steps[6]$content[1])
+jsonLength = length(steps[6]$content)
+for(i in 2:jsonLength) {
+  myJson <- paste(myJson, rawToChar(steps[6]$content[i]), sep="")
+}
+
+# Saving the json string (just to reopen it later - a better way?)
+cat(myJson, file="mySteps.dat")
 mySteps = fromJSON("mySteps.dat")
